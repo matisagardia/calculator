fe41c002db8f4856912aaaeeb330bf75b4663055
matisagardia <matisagardia7@gmail.com>
2022-09-16 18:30:37 -0300

    Variables and basic functions

--- main.js
+++ main.js
@@ -1,40 +1,52 @@
-let a = 5;
-let b = 15;
+//Variable declarations
+let currentNum = "";
+let previousNum = "";
+let operator = "";
 
 
-function add(a, b) {
-    return a + b;
+const entryValue = document.querySelector(".entryValue");
+const savedValue = document.querySelector(".savedValue");
+
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+const clear = document.querySelector(".clear");
+
+const equal = document.querySelector(".equal");
+
+const erase = document.querySelector(".erase");
+
+const dot = document.querySelector(".dot");
+
+
+//Eventlisteners for number clicks. forEach because querySelectorAll returns an array. pushNumber is a function that stores is currentNum the number that I click on the html and then
+//sets on the entryValue field that number.
+
+
+numbers.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        pushNumber(e.target.textContent);
+    });
+});
+
+function pushNumber(number) {
+    if(currentNum.length < 12){
+    currentNum += number;
+    entryValue.textContent = currentNum;
 }
-function substract(a, b) {
-    return a - b;
 }
 
-function divide(a, b) {
-    return a / b;
+//Eventlisteners for operator clicks.
+
+operators.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        pushOperator(e.target.textContent);
+    })
+})
+
+function pushOperator(operat) {
+    if(currentNum.length < 12){
+        currentNum += operat;
+        entryValue.textContent = currentNum;
+    }
 }
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function rest(a, b) {
-    return a % b;
-}
-
-function erase() {
-
-}
-
-function clear() {
-
-}
-
-function operate() {
-
-}
-
-add();
-substract();
-divide();
-multiply();
-rest();
