4f6fda6d24f92995b1f02dcbf0e60b67cecf4221
matisagardia <matisagardia7@gmail.com>
2022-09-16 16:29:10 -0300

    pusheada pal fresa

---
+++ main.js
@@ -0,0 +1,40 @@
+let a = 5;
+let b = 15;
+
+
+function add(a, b) {
+    return a + b;
+}
+function substract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function rest(a, b) {
+    return a % b;
+}
+
+function erase() {
+
+}
+
+function clear() {
+
+}
+
+function operate() {
+
+}
+
+add();
+substract();
+divide();
+multiply();
+rest();
